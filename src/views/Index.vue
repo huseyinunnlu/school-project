78ea784032a23c78eaf9d3298479aa4670af0bfd
huseyinunnlu <huseyinunnlu@gmail.com>
2021-09-28 16:05:48 +0300

    Adminpanel assets installed

---
+++ src/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>
+            Test
+            <button class="btn btn-primary"></button>
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
